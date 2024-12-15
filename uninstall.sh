073e9e3c46eb4c891e06b81e135d2d0a9c72fe82
Joan Mendoza <wadalino@gmail.com>
2024-12-16 00:50:35 +0100

    uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+systemctl stop mqtt-ha.service
+systemctl disable mqtt-ha.service
+rm -rf /etc/systemd/system/mqtt-ha.service
+systemctl daemon-reload
+
+rm -rf /root/ha-mqtt-service
+rm -rf /root/outFiles
